376859d7c017832b2793cc0be2bb3c7ce05698cb
tahamannai <taha.mannai@hotmail.com>
2016-04-27 16:41:42 +0200

    fizz method added

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,12 @@
 def fizz_buzz(number)
   number
 end
+def fizz_buzz(number)
+  case
+    when divisible_by_three?(number) then 'fizz'
+    else number
+  end
+end
+def divisible_by_three?(number)
+  number % 3 == 0
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -4,5 +4,7 @@ describe 'fizz_buzz' do
   it 'returns 1 if number = 1' do
       expect(fizz_buzz(1)).to eq 1
   end
-
+  it "returns 'fizz' if number is divisible by 3" do
+    expect(fizz_buzz(3)).to eq 'fizz'
+  end
 end
