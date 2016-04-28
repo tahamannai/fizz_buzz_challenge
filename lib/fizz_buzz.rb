a8e0d0d16c4b23cde77580cb9fdb80275b5910fe
tahamannai <taha.mannai@hotmail.com>
2016-04-28 12:50:09 +0200

    made all tests pass

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,9 +1,21 @@
 def fizz_buzz(number)
   case
+    when divisible_by_fifteen?(number) then 'fizzbuzz'
+    when divisible_by_five?(number) then 'buzz'
     when divisible_by_three?(number) then 'fizz'
-    else number
+
+  else number
   end
 end
+
 def divisible_by_three?(number)
   number % 3 == 0
+end
+
+def divisible_by_five?(number)
+  number % 5 == 0
+end
+
+def divisible_by_fifteen?(number)
+  number % 15 == 0
 end
